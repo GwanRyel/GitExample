6f9773d2a550312c9bcaec2e9a9ffaddde6c99cc
GwanRyel <bos_1016@naver.com>
2018-10-20 19:03:18 +0900

    created test file

---
+++ Autolayout_Basic_1/Autolayout_Basic_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout_Basic_1
+//
+//  Created by 이관렬 on 30/09/2018.
+//  Copyright © 2018 이관렬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
